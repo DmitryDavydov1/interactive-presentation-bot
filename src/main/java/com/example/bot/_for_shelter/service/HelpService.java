274dccb4bad95c91a7a50f9e71ae0f42d5624509
Dmitry davydov <dima2007game@gmail.com>
2025-04-01 19:47:13 +0300

    добавил кеширование

--- src/main/java/com/example/bot/_for_shelter/service/HelpService.java
+++ src/main/java/com/example/bot/_for_shelter/service/HelpService.java
@@ -2,12 +2,15 @@ package com.example.bot._for_shelter.service;
 
 import com.example.bot._for_shelter.models.CreatorTheRoom;
 import com.example.bot._for_shelter.models.Room;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class HelpService {
+
+    @Cacheable(value = "rooms", key = "#creatorTheRoom.id")
     public Room findLastRoom(CreatorTheRoom creatorTheRoom) {
         List<Room> rooms = creatorTheRoom.getRoom();
 
@@ -15,7 +18,6 @@ public class HelpService {
                 .filter(Room::isStatus) // Фильтруем по статусу
                 .findFirst().orElse(null);
     }
-
 
     public String test() {
         return "test";
